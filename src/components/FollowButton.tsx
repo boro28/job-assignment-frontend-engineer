7ef8887cfbdf32da5c7f3836c334e98c1d8bf5a0
boro28 <borkowskiszymon28@gmail.com>
2025-02-19 00:17:22 +0100

    Wip

--- src/components/FollowButton.tsx
+++ src/components/FollowButton.tsx
@@ -1,16 +1,17 @@
 import { Profile } from "../api/Api";
 import { useState } from "react";
 import { followProfile } from "../api/profiles";
+import { useIsLoggedIn } from "../storage/auth";
 
 export default function FollowButton({
   username,
   following: followingProp,
 }: Pick<Profile, "username" | "following">): JSX.Element {
-  //TODO: add functionality
+  const isLoggedIn = useIsLoggedIn();
   const [following, setFollowing] = useState<boolean>(followingProp);
   return (
     <button
-      className={"btn btn-sm btn-outline-secondary action-btn" + following ? " active" : ""}
+      className={"btn btn-sm btn-outline-secondary action-btn " + (following ? " active" : "")}
       onClick={() => followProfile(username).then(() => setFollowing(true))}
     >
       <i className="ion-plus-round" />
